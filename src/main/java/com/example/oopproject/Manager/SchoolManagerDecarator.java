85301d764c7acb9c6862b7776760c5bc30f0c0b4
Zhanel <122990780+zhanel01@users.noreply.github.com>
2023-12-14 00:31:46 +0600

    Add files via upload

---
+++ src/main/java/com/example/oopproject/Manager/SchoolManagerDecarator.java
@@ -0,0 +1,11 @@
+package manager;
+
+public class SchoolManagerDecarator {
+	
+	public void assignCourseToTeacher() {
+        // Логика для назначения курса преподавателю
+        // ...
+        System.out.println("Assigning course to teacher...");
+    }
+
+}
