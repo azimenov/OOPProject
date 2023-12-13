1004dfbe250ed2d69f6fe509405bb2dae70ba278
Zhanel <122990780+zhanel01@users.noreply.github.com>
2023-12-14 00:41:01 +0600

    User commit

---
+++ src/main/java/com/example/oopproject/ManagerPackege/ORManagerDecorator.java
@@ -0,0 +1,22 @@
+package manager;
+
+
+public class ORManagerDecorator extends ManagerDecorator {
+	
+	public void manageSchedule() {
+        
+        System.out.println("Managing schedule...");
+    }
+
+    public void confirmRegistration() {
+        
+        System.out.println("Confirming registration...");
+    }
+    
+    
+    
+
+    
+  
+    
+}
