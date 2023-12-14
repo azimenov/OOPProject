f72b27b79275ce5924fc0e16c178aff1bf38d05e
Zhanel <122990780+zhanel01@users.noreply.github.com>
2023-12-14 21:48:29 +0600

    Update ORManagerDecorator.java

--- src/main/java/com/example/oopproject/ManagerPackege/ORManagerDecorator.java
+++ src/main/java/com/example/oopproject/ManagerPackege/ORManagerDecorator.java
@@ -1,22 +1,45 @@
 package manager;
 
+import java.util.HashMap;
+import java.util.Map;
 
 public class ORManagerDecorator extends ManagerDecorator {
-	
-	public void manageSchedule() {
-        
-        System.out.println("Managing schedule...");
+    private ManagerDecoratorType type;
+
+    public ORManagerDecorator(ManagerDecoratorType type) {
+        this.type = type;
     }
 
-    public void confirmRegistration() {
-        
-        System.out.println("Confirming registration...");
+    public boolean registerForCourse(Student student, String courseCode, Teacher teacher) {
+        Course c = Course.findCourse(courseCode);
+        if (c != null && student.enrollCourse(c)) {
+            if (!student.getAllCourses().containsKey(c)) {
+                student.getAllCourses().put(c, new Mark());
+                teacher.addStudent(c, student);
+                return true;
+            }
+        }
+        return false;
     }
-    
-    
-    
 
     
-  
-    
+
+    public boolean deleteFromCourse(Student student, String courseCode) {
+        Course c = Course.findCourse(courseCode);
+        HashMap<Course, Mark> allCourses = student.getAllCourses();
+        for (Iterator<Map.Entry<Course, Mark>> iterator = allCourses.entrySet().iterator(); iterator.hasNext();) {
+            Map.Entry<Course, Mark> entry = iterator.next();
+            if (entry.getKey().equals(c)) {
+                iterator.remove();
+                student.setAllCourses(allCourses);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " " + this.type;
+    }
 }
