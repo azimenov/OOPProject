77b2e6975f5c8c7221dfc66f4b63a8514ba749ce
Zhorik300 <123461894+Zhorik300@users.noreply.github.com>
2023-12-15 10:00:01 +0600

    Add files via upload

---
+++ src/main/java/com/example/oopproject/ResearcherPackage/src/ResearcherPackage/Researcher.java
@@ -0,0 +1,33 @@
+package ResearcherPackage;
+package com.example.oopproject.researcher;
+import com.example.oopproject.UserPackage.User;
+import com.example.oopproject.UserPackage.enums.FamilyStatus;
+import com.example.oopproject.UserPackage.enums.Gender;
+
+class Researcher extends User{
+    private String name;
+    private ResearchProject project;
+    private List<ResearchPaper> publications;
+
+    public Researcher(String name) {
+        this.name = name;
+        this.project = new ResearchProject("Default Project");
+        this.publications = new ArrayList<>();
+    }
+
+    public ResearchProject getProject() {
+        return project;
+    }
+
+    public void setProject(ResearchProject project) {
+        this.project = project;
+    }
+
+    public void publish(ResearchPaper paper) {
+        publications.add(paper);
+    }
+
+    public void delete(ResearchPaper paper) {
+        publications.remove(paper);
+    }
+}
