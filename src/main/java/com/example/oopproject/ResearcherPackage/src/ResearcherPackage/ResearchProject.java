567da345ee2765f0cb429c4d604b0aa619cbb9b7
Zhorik300 <123461894+Zhorik300@users.noreply.github.com>
2023-12-15 10:05:19 +0600

    Update ResearchProject.java

--- src/main/java/com/example/oopproject/ResearcherPackage/src/ResearcherPackage/ResearchProject.java
+++ src/main/java/com/example/oopproject/ResearcherPackage/src/ResearcherPackage/ResearchProject.java
@@ -1,5 +1,24 @@
-package src;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ResearchProject {
+    private String title;
+    private List<Researcher> members;
 
-}
+    public ResearchProject(String title) {
+        this.title = title;
+        this.members = new ArrayList<>();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void addMember(Researcher researcher) {
+        members.add(researcher);
+    }
+
+    public List<Researcher> getMembers() {
+        return members;
+    }
