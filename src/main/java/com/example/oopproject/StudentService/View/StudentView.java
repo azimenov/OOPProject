c6ab74caaee86bcb4b28d422edab7e430ee45ee2
Zhorik300 <123461894+Zhorik300@users.noreply.github.com>
2023-12-24 19:25:41 +0600

    Update StudentView.java

--- src/main/java/com/example/oopproject/StudentService/View/StudentView.java
+++ src/main/java/com/example/oopproject/StudentService/View/StudentView.java
@@ -1,31 +1,18 @@
-package com.example.oopproject.StudentService.View;
+package com.example.oopproject.StudentService.view;
 
-import com.example.oopproject.StudentService.controller.StudentController;
 import com.example.oopproject.StudentService.model.Student;
-
-import java.util.Scanner;
+import com.example.oopproject.StudentService.controller.StudentController;
 
 public class StudentView {
+    protected Student student;
     protected StudentController studentController;
-    protected Scanner sc;
 
-    public StudentView(StudentController studentController){
+    public StudentView(Student student, StudentController studentController) {
+        this.student = student;
         this.studentController = studentController;
-        sc = new Scanner(System.in);
     }
-    public void getView() {
-        System.out.println("1:View Courses\n2:log out");
 
-        System.out.print("Choose: ");
-        int option = sc.nextInt();
-        if (option == 1) {
-            studentController.viewCourses();
-        }
-        else if (option == 2) {
-            return;
-        }
-        else{
-            getView();
-        }
+    public void getDefaultView(){
+        System.out.print("Welcome to system\n1. View Courses\n2. View Courses\n3. View Teachers");
     }
 }
