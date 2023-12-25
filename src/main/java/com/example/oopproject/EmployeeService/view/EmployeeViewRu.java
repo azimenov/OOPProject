cca9aa87027bd74a24912678c772ba7a1f1b6f87
Zhorik300 <123461894+Zhorik300@users.noreply.github.com>
2023-12-26 00:19:42 +0600

    Add files via upload

---
+++ src/main/java/com/example/oopproject/EmployeeService/view/EmployeeViewRu.java
@@ -0,0 +1,63 @@
+import com.example.oopproject.EmployeeService.controller.EmployeeController;
+import com.example.oopproject.EmployeeService.model.Employee;
+
+import java.util.Scanner;
+
+public class EmployeeView {
+    protected EmployeeController employeeController;
+    protected Employee employee;
+    private Scanner scanner;
+
+    public EmployeeView(EmployeeController employeeController, Employee employee, Scanner scanner) {
+        this.employeeController = employeeController;
+        this.employee = employee;
+        this.scanner = scanner;
+    }
+
+    public EmployeeView() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void getView() {
+        System.out.println("Добро пожаловать в систему\n1. Смотреть сообщения\n2. Просмотреть все сообщения\n3. Отправлять сообщения");
+
+        int option = scanner.nextInt();
+
+        switch (option) {
+            case 1:
+                seeMessages();
+                break;
+            case 2:
+                seeUnreadMessages();
+                break;
+            case 3:
+                sendMessage();
+                break;
+            default:
+                System.out.println("Неверный выбор");
+        }
+    }
+
+    public void sendMessage() {
+        scanner.nextLine(); 
+
+        System.out.print("Введите ваше сообщение: ");
+        String messageText = scanner.nextLine();
+
+      
+        employeeController.sendMessage(employee, messageText);
+        System.out.println("Сообщение отправлено!");
+    }
+
+    public void seeMessages() {
+        for (Message message : employeeController.getUnreadMessages(employee)) {
+            System.out.println(message);
+        }
+    }
+
+    public void seeUnreadMessages() {
+        for (Message message : employeeController.getAllMessages(employee)) {
+            System.out.println(message);
+        }
+    }
+}
