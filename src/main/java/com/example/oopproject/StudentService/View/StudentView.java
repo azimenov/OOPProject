81c91d10ef817f01112129a7ad2356a36d0b92b5
Zhorik300 <123461894+Zhorik300@users.noreply.github.com>
2023-12-25 19:07:42 +0600

    Update StudentView.java

--- src/main/java/com/example/oopproject/StudentService/View/StudentView.java
+++ src/main/java/com/example/oopproject/StudentService/View/StudentView.java
@@ -3,28 +3,52 @@ package com.example.oopproject.StudentService.View;
 import com.example.oopproject.StudentService.model.Student;
 import com.example.oopproject.StudentService.controller.StudentController;
 
+import java.util.Scanner;
+
 public class StudentView {
-    protected Student student;
-    protected StudentController studentController;
+    private final StudentController studentController;
+    private final Scanner scanner;
 
-    public StudentView(Student student, StudentController studentController) {
-        this.student = student;
+    public StudentView(StudentController studentController, Scanner scanner) {
         this.studentController = studentController;
+        this.scanner = scanner;
     }
 
-    public void getDefaultView(){
-        System.out.print("Welcome to system\n1. View Courses\n2. View Courses\n3. View Teachers");
+    public void getDefaultView() {
+        System.out.println("Welcome to system");
+        System.out.println("1. View Courses");
+        System.out.println("2. View Teachers");
+        System.out.println("3. View Marks");
     }
 
-    public void seeCourses(){
-        studentController.getAllCourses(student);
+    public void seeCourses() {
+        System.out.println("Your Courses:");
+        HashSet<String> courses = studentController.getAllCourses(student);
+        for (String course : courses) {
+            System.out.println(course);
+        }
     }
 
-    public void viewInfoAboutTeacher(){
-        studentController.viewInfoAboutTeacher(student, coursename)
+    public void viewInfoAboutTeacher() {
+        System.out.print("Enter course name: ");
+        String courseName = scanner.nextLine();
+
+        String teacherInfo = studentController.viewInfoAboutTeacher(student, courseName);
+        if (teacherInfo != null) {
+            System.out.println(teacherInfo);
+        } else {
+            System.out.println("Teacher information not found for this course.");
+        }
     }
 
-    public void viewMarks(){
-        studentController.viewMarks(student);
+    public void viewMarks() {
+        System.out.println("Your Marks:");
+        Vector<Mark> marks = studentController.viewMarks(student);
+        for (Mark mark : marks) {
+            System.out.println(mark);
+        }
     }
 }
+
+
+
