20988171f569d89f5e4f2e30b4f1ac1772c212d3
Zhanel <122990780+zhanel01@users.noreply.github.com>
2023-12-26 23:01:29 +0600

    Update StudentViewFactory.java

--- src/main/java/com/example/oopproject/Factory/StudentViewFactory.java
+++ src/main/java/com/example/oopproject/Factory/StudentViewFactory.java
@@ -1,4 +1,4 @@
-package com.example.oopproject.UserPackage;
+package com.example.oopproject.Factory;
 
 import com.example.oopproject.StudentService.Student;
 import com.example.oopproject.StudentService.controller.StudentController;
@@ -6,8 +6,18 @@ import com.example.oopproject.StudentService.view.StudentViewEn;
 import com.example.oopproject.StudentService.view.StudentViewKz;
 import com.example.oopproject.StudentService.view.StudentViewRu;
 
+/**
+ * Factory class to create StudentView based on language options.
+ */
 public class StudentViewFactory {
 
+    /**
+     * Returns a StudentView based on the provided option.
+     * @param option The option to select the StudentView language.
+     * @param student The Student instance.
+     * @param studentController The StudentController instance.
+     * @return The appropriate StudentView based on the option provided.
+     */
     public StudentView getView(int option, Student student, StudentController studentController){
         if(option == 1){
             return new StudentViewEn(student, studentController);
@@ -20,3 +30,4 @@ public class StudentViewFactory {
         }
     }
 }
+
