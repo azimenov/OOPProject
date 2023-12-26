42bf0a1fa9adda7649c346ed56d13fa23b46fb54
Baisal <123240140+baimanbk@users.noreply.github.com>
2023-12-26 22:32:06 +0600

    Update GraduateStudent.java

--- src/main/java/com/example/oopproject/StudentService/model/GraduateStudent.java
+++ src/main/java/com/example/oopproject/StudentService/model/GraduateStudent.java
@@ -6,20 +6,29 @@ import com.example.oopproject.UserPackage.enums.Role;
 import com.example.oopproject.StudentService.enums.Faculty;
 import com.example.oopproject.StudentService.enums.Organization;
 
+// GraduateStudent class extending Student class
 class GraduateStudent extends Student {
-	public GraduateStudent(String id, String password, String firstName, String lastName, String phoneNumber, Gender gender, FamilyStatus familyStatus, Role role, Faculty faculty, int yearOfStudy, Organization organization) {
-		super(id, password, firstName, lastName, phoneNumber, gender, familyStatus, role, faculty, yearOfStudy, organization);
-	}
 
-	public void getSchedule() {
-		return ;
-	}
+    // Constructor for initializing GraduateStudent
+    public GraduateStudent(String id, String password, String firstName, String lastName, String phoneNumber, Gender gender, FamilyStatus familyStatus, Role role, Faculty faculty, int yearOfStudy, Organization organization) {
+        super(id, password, firstName, lastName, phoneNumber, gender, familyStatus, role, faculty, yearOfStudy, organization);
+    }
 
+    // Method to get the schedule of the graduate student
+    public void getSchedule() {
+        // Method implementation goes here
+        // Possibly retrieve and handle the schedule
+    }
 
-	public void viewDiplomaMark() {
-	     
-	}
-	public void getView() {
-		return ;
-	}
+    // Method to view diploma mark of the graduate student
+    public void viewDiplomaMark() {
+        // Method implementation goes here
+        // Logic to view the diploma mark
+    }
+
+    // Method to perform a view action (but currently returning void)
+    public void getView() {
+        return; // This method currently returns void
+        // Implement logic or change return type if needed
+    }
 }
