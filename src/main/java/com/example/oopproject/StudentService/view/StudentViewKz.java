2bb07fb42d2d09836a71639fae038c7b80aa1785
Baisal <123240140+baimanbk@users.noreply.github.com>
2023-12-26 22:40:08 +0600

    Update StudentViewKz.java

--- src/main/java/com/example/oopproject/StudentService/view/StudentViewKz.java
+++ src/main/java/com/example/oopproject/StudentService/view/StudentViewKz.java
@@ -5,22 +5,26 @@ import com.example.oopproject.StudentService.model.Mark;
 import com.example.oopproject.StudentService.model.Student;
 
 import java.util.HashSet;
-import java.util.Scanner;
 import java.util.Vector;
 
-public class StudentViewKz extends StudentViewEn{
+// Kazakh language view class extending the English view
+public class StudentViewKz extends StudentViewEn {
 
+    // Constructor initializing with a StudentController and a Student
     public StudentViewKz(StudentController studentController, Student student) {
         super(studentController, student);
     }
 
+    // Method to display the Kazakh view options
     private void displayKazakhView() {
-       System.out.println("Жүйеге қош келдіңіз");
+        System.out.println("Жүйеге қош келдіңіз");
         System.out.println("1. Курстарды шолу");
         System.out.println("2. Мұғалімдерді шолу");
         System.out.println("3. Бағаларды шолу");
     }
 
+    // Overridden method to display courses in Kazakh language
+    @Override
     public void seeCourses() {
         System.out.println("Сіздің курстарыңыз:");
         HashSet<String> courses = studentController.getAllCourses(student);
@@ -29,6 +33,8 @@ public class StudentViewKz extends StudentViewEn{
         }
     }
 
+    // Overridden method to view teacher information in Kazakh language
+    @Override
     public void viewInfoAboutTeacher() {
         System.out.print("Курс атын енгізіңіз: ");
         String courseName = scanner.nextLine();
@@ -41,6 +47,8 @@ public class StudentViewKz extends StudentViewEn{
         }
     }
 
+    // Overridden method to view marks in Kazakh language
+    @Override
     public void viewMarks() {
         System.out.println("Сіздің бағаларыңыз:");
         Vector<Mark> marks = studentController.viewMarks(student);
