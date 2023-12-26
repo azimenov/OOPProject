d76aaf12754b25d8a52a3551fe9b8103876c822a
Baisal <123240140+baimanbk@users.noreply.github.com>
2023-12-26 22:39:01 +0600

    Update StudentViewEn.java

--- src/main/java/com/example/oopproject/StudentService/view/StudentViewEn.java
+++ src/main/java/com/example/oopproject/StudentService/view/StudentViewEn.java
@@ -8,36 +8,38 @@ import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Vector;
 
+// View class for English language
 public class StudentViewEn {
-    protected final StudentController studentController;
-    protected final Scanner scanner;
-    protected Student student;
+    protected final StudentController studentController; // Controller to interact with student data
+    protected final Scanner scanner; // Scanner for user input
+    protected Student student; // Current student
 
+    // Constructor initializing with a StudentController and a Student
     public StudentViewEn(StudentController studentController, Student student) {
         this.studentController = studentController;
         scanner = new Scanner(System.in);
-        this.student =student;
+        this.student = student;
     }
 
+    // Method to display the default view options
     public void getDefaultView() {
-        System.out.println("Welcome to system");
+        System.out.println("Welcome to the system");
         System.out.println("1. View Courses");
         System.out.println("2. View Teachers");
         System.out.println("3. View Marks");
 
-        System.out.print("Select option: ");
+        System.out.print("Select an option: ");
         int option = scanner.nextInt();
-        if(option == 1){
+        if (option == 1) {
             seeCourses();
-        }
-        else if(option == 2){
+        } else if (option == 2) {
             viewInfoAboutTeacher();
-        }
-        else if(option == 3){
+        } else if (option == 3) {
             viewMarks();
         }
     }
 
+    // Method to display the courses of the student
     public void seeCourses() {
         System.out.println("Your Courses:");
         HashSet<String> courses = studentController.getAllCourses(student);
@@ -46,6 +48,7 @@ public class StudentViewEn {
         }
     }
 
+    // Method to view information about a teacher for a specific course
     public void viewInfoAboutTeacher() {
         System.out.print("Enter course name: ");
         String courseName = scanner.next();
@@ -58,6 +61,7 @@ public class StudentViewEn {
         }
     }
 
+    // Method to view the marks of the student
     public void viewMarks() {
         System.out.println("Your Marks:");
         Vector<Mark> marks = studentController.viewMarks(student);
