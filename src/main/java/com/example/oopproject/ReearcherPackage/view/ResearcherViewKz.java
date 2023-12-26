64e5627dad173839a45974b31f9110bb732ac601
Baisal <123240140+baimanbk@users.noreply.github.com>
2023-12-26 22:54:09 +0600

    Update ResearcherViewKz.java

--- src/main/java/com/example/oopproject/ReearcherPackage/view/ResearcherViewKz.java
+++ src/main/java/com/example/oopproject/ReearcherPackage/view/ResearcherViewKz.java
@@ -4,25 +4,28 @@ import com.example.oopproject.ResearcherPackage.controller.ResearcherController;
 import com.example.oopproject.ResearcherPackage.model.ResearchPaper;
 import com.example.oopproject.ResearcherPackage.model.Researcher;
 
-import java.util.Comparator;
 import java.util.Scanner;
 
+// Represents the view for a Researcher in Kazakh extending ResearcherViewEn
 public class ResearcherViewKz extends ResearcherViewEn {
 
+    // Constructor to initialize ResearcherViewKz
     public ResearcherViewKz(ResearcherController researcherController, Researcher researcher) {
-        super(researcherController,researcher);
+        super(researcherController, researcher);
     }
 
+    // Method to display the default view for a researcher in Kazakh
+    @Override
     public void getDefaultView() {
-       System.out.println("«Зерттеуші жүйесіне қош келдіңіз»");
-         System.out.println("1. Қағазды жариялау");
-         System.out.println("2. Қағазды жою");
-         System.out.println("3. Қағаздарды басып шығару");
-         System.out.println("4. H-индексін есептеу");
+        System.out.println("«Зерттеуші жүйесіне қош келдіңіз»");
+        System.out.println("1. Қағазды жариялау");
+        System.out.println("2. Қағазды жою");
+        System.out.println("3. Қағаздарды басып шығару");
+        System.out.println("4. H-индексін есептеу");
 
         System.out.print("Опцияны таңдаңыз: ");
         int option = scanner.nextInt();
-        scanner.nextLine(); 
+        scanner.nextLine(); // Consumes the newline character after reading the integer input
 
         switch (option) {
             case 1:
@@ -31,10 +34,10 @@ public class ResearcherViewKz extends ResearcherViewEn {
                 researcherController.publishPaper(new ResearchPaper(title));
                 break;
             case 2:
-              
+                // Code to delete a paper
                 break;
             case 3:
-              
+                // Code to print papers
                 break;
             case 4:
                 int hIndex = researcherController.calculateHIndex();
