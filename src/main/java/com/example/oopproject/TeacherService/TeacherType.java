149d643fe04e1daa60d44e3c43cf91a45f020cec
Zhanel <122990780+zhanel01@users.noreply.github.com>
2023-12-26 23:16:53 +0600

    Update TeacherType.java

--- src/main/java/com/example/oopproject/TeacherService/TeacherType.java
+++ src/main/java/com/example/oopproject/TeacherService/TeacherType.java
@@ -1,9 +1,12 @@
 package com.example.oopproject.TeacherService;
 
+/**
+ * Enum representing different types of teachers.
+ */
 public enum TeacherType {
-    LECTURE,
-    ASSISTENT,
-    PROFESSOR,
-    SENIORLECTURE,
-    TUTOR;
+    LECTURE,         // Лектор
+    ASSISTENT,       // Ассистент
+    PROFESSOR,       // Профессор
+    SENIORLECTURE,   // Старший лектор
+    TUTOR;           // Репетитор
 }
