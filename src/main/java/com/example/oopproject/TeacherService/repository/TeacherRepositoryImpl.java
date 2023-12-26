9fe6b61ef4c2ddf50fb3f6cb6c517224618caeac
Zhanel <122990780+zhanel01@users.noreply.github.com>
2023-12-26 23:19:30 +0600

    Update TeacherRepositoryImpl.java

--- src/main/java/com/example/oopproject/TeacherService/repository/TeacherRepositoryImpl.java
+++ src/main/java/com/example/oopproject/TeacherService/repository/TeacherRepositoryImpl.java
@@ -8,26 +8,50 @@ import com.example.oopproject.db.DataBase;
 
 import java.util.List;
 
-public class TeacherRepositoryImpl extends EmployeeRepositoryImpl implements TeacherRepository{
+public class TeacherRepositoryImpl extends EmployeeRepositoryImpl implements TeacherRepository {
     public TeacherRepositoryImpl(DataBase dataBase) {
         super(dataBase);
     }
 
+    /**
+     * Finds a teacher by email.
+     * @param email The email of the teacher.
+     * @return The Teacher object if found, null otherwise.
+     */
     @Override
     public Teacher findTeacherByEmail(String email) {
-        return (Teacher) dataBase.getEmployeeDatabase().stream().filter(user -> user.getEmail().equals(email) && user.getRole() == Role.Teacher).findFirst().orElse(null);
+        return (Teacher) dataBase.getEmployeeDatabase().stream()
+                .filter(user -> user.getEmail().equals(email) && user.getRole() == Role.Teacher)
+                .findFirst().orElse(null);
     }
 
+    /**
+     * Finds a teacher by ID.
+     * @param id The ID of the teacher.
+     * @return The Teacher object if found, null otherwise.
+     */
     @Override
     public Teacher findTeacherById(String id) {
-        return (Teacher) dataBase.getEmployeeDatabase().stream().filter(user -> user.getId().equals(id) && user.getRole() == Role.Teacher).findFirst().orElse(null);
+        return (Teacher) dataBase.getEmployeeDatabase().stream()
+                .filter(user -> user.getId().equals(id) && user.getRole() == Role.Teacher)
+                .findFirst().orElse(null);
     }
 
+    /**
+     * Retrieves a list of all teachers.
+     * @return A list containing all the teachers.
+     */
     @Override
     public List<Employee> getAllTeachers() {
-        return dataBase.getEmployeeDatabase().stream().filter(user->user.getRole() == Role.Teacher).toList();
+        return dataBase.getEmployeeDatabase().stream()
+                .filter(user -> user.getRole() == Role.Teacher)
+                .toList();
     }
 
+    /**
+     * Adds a new teacher to the repository.
+     * @param teacher The Teacher object to be added.
+     */
     @Override
     public void addTeacher(Teacher teacher) {
         super.addEmployee(teacher);
