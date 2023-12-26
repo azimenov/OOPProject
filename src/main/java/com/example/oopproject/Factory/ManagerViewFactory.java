42eac3310f9bb1d8724193e01eb44e8f8b865a7f
Zhanel <122990780+zhanel01@users.noreply.github.com>
2023-12-26 22:55:50 +0600

    Update ManagerViewFactory.java

--- src/main/java/com/example/oopproject/Factory/ManagerViewFactory.java
+++ src/main/java/com/example/oopproject/Factory/ManagerViewFactory.java
@@ -6,8 +6,18 @@ import com.example.oopproject.ManagerService.view.ManagerViewEn;
 import com.example.oopproject.ManagerService.view.ManagerViewKz;
 import com.example.oopproject.ManagerService.view.ManagerViewRu;
 
+/**
+ * Factory class to create ManagerView based on language options.
+ */
 public class ManagerViewFactory {
 
+    /**
+     * Returns a ManagerView based on the provided option.
+     * @param option The option to select the ManagerView language.
+     * @param manager The Manager instance.
+     * @param managerController The ManagerController instance.
+     * @return The appropriate ManagerView based on the option provided.
+     */
     public ManagerViewEn getView(int option, Manager manager, ManagerController managerController){
         if(option == 1){
             return new ManagerViewEn(manager, managerController);
