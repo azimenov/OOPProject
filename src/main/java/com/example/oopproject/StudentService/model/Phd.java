289dbd96f2b2945e0517e0e870b7e833b40003f4
Baisal <123240140+baimanbk@users.noreply.github.com>
2023-12-26 22:34:34 +0600

    Update Phd.java

--- src/main/java/com/example/oopproject/StudentService/model/Phd.java
+++ src/main/java/com/example/oopproject/StudentService/model/Phd.java
@@ -6,17 +6,21 @@ import com.example.oopproject.UserPackage.enums.Role;
 import com.example.oopproject.StudentService.enums.Faculty;
 import com.example.oopproject.StudentService.enums.Organization;
 
+// Phd class extending GraduateStudent
 class Phd extends GraduateStudent {
 
-	public Phd(String id, String password, String firstName, String lastName, String phoneNumber, Gender gender, FamilyStatus familyStatus, Role role, Faculty faculty, int yearOfStudy, Organization organization) {
-		super(id, password, firstName, lastName, phoneNumber, gender, familyStatus, role, faculty, yearOfStudy, organization);
-	}
+    // Constructor for initializing Phd
+    public Phd(String id, String password, String firstName, String lastName, String phoneNumber, Gender gender, FamilyStatus familyStatus, Role role, Faculty faculty, int yearOfStudy, Organization organization) {
+        super(id, password, firstName, lastName, phoneNumber, gender, familyStatus, role, faculty, yearOfStudy, organization);
+    }
 
-	public void defendScientificReport() {
-	     
-	 }
+    // Method representing the defense of a scientific report by a Ph.D. student
+    public void defendScientificReport() {
+        // Implement logic for defending a scientific report
+    }
 
-	 public void createResearcher() {
-	    
-	 }
+    // Method for creating a researcher (possibly related to a Ph.D. program)
+    public void createResearcher() {
+        // Implement logic for creating a researcher
+    }
 }
