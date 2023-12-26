b62c8e710d39720fb968239bedf55ee6081eccc0
Baisal <123240140+baimanbk@users.noreply.github.com>
2023-12-26 22:37:29 +0600

    Update StudentRepositoryImpl.java

--- src/main/java/com/example/oopproject/StudentService/repository/StudentRepositoryImpl.java
+++ src/main/java/com/example/oopproject/StudentService/repository/StudentRepositoryImpl.java
@@ -5,29 +5,45 @@ import com.example.oopproject.db.DataBase;
 
 import java.util.List;
 
+// Implementation of StudentRepository interface
 public class StudentRepositoryImpl implements StudentRepository {
-    public StudentRepositoryImpl(DataBase dataBase){
+
+    private final DataBase dataBase; // Database to store student data
+
+    // Constructor initializing with a DataBase object
+    public StudentRepositoryImpl(DataBase dataBase) {
         this.dataBase = dataBase;
     }
-    private final DataBase dataBase;
+
+    // Finds a student by email
     @Override
-    public Student findStudentByEmail(String email){
-        return (Student)dataBase.getStudentDatabase().stream().filter(student -> student.getEmail().equals(email)).findFirst().orElse(null);
+    public Student findStudentByEmail(String email) {
+        // Filter the student database by email and return the first match or null
+        return (Student) dataBase.getStudentDatabase().stream()
+                .filter(student -> student.getEmail().equals(email))
+                .findFirst()
+                .orElse(null);
     }
 
+    // Finds a student by ID
     @Override
     public Student findStudentById(String Id) {
-        return (Student)dataBase.getStudentDatabase().stream().filter(student -> student.getId().equals(Id)).findFirst().orElse(null);
-
+        // Filter the student database by ID and return the first match or null
+        return (Student) dataBase.getStudentDatabase().stream()
+                .filter(student -> student.getId().equals(Id))
+                .findFirst()
+                .orElse(null);
     }
 
+    // Adds a student to the database
     @Override
     public void addStudent(Student student) {
         dataBase.getStudentDatabase().add(student);
     }
 
+    // Retrieves all students from the database
     @Override
     public List<Student> getAllStudent() {
-        return dataBase.getStudentDatabase().stream().toList();
+        return dataBase.getStudentDatabase(); // Returns the list of students from the database
     }
 }
