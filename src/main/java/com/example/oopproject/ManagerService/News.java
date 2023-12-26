12306de587d3bfd3602572f468a70aa1fd64d773
Zhanel <122990780+zhanel01@users.noreply.github.com>
2023-12-26 22:53:39 +0600

    Update News.java

--- src/main/java/com/example/oopproject/ManagerService/News.java
+++ src/main/java/com/example/oopproject/ManagerService/News.java
@@ -3,26 +3,40 @@ package com.example.oopproject.ManagerService;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * News class manages a list of news items.
+ */
 public class News {
     private List<String> newsList;
 
+    /**
+     * Constructor initializes the news list as an empty ArrayList.
+     */
     public News() {
         this.newsList = new ArrayList<>();
     }
 
-    // Метод для добавления новости
+    /**
+     * Adds a news item to the news list.
+     * @param news The news item to be added.
+     */
     public void addNews(String news) {
         newsList.add(news);
     }
 
-    // Метод для удаления новости
+    /**
+     * Removes a news item from the news list.
+     * @param news The news item to be removed.
+     */
     public void removeNews(String news) {
         newsList.remove(news);
     }
 
-    // Метод для получения списка новостей
+    /**
+     * Retrieves the list of news items.
+     * @return The list of news items.
+     */
     public List<String> getNewsList() {
         return newsList;
     }
 }
-
