5d9822c8f231b4acc6d57a18963be8ff5d02b891
Zhanel <122990780+zhanel01@users.noreply.github.com>
2023-12-26 23:17:48 +0600

    Update TeacherViewKz.java

--- src/main/java/com/example/oopproject/TeacherService/view/TeacherViewKz.java
+++ src/main/java/com/example/oopproject/TeacherService/view/TeacherViewKz.java
@@ -12,12 +12,23 @@ import java.util.Map;
 import java.util.Scanner;
 import java.util.Vector;
 
+/**
+ * Represents the view of a Teacher in the system with Kazakh language settings.
+ */
 public class TeacherViewKz extends TeacherViewEn {
 
+    /**
+     * Constructs a TeacherViewKz object with the provided Teacher and TeacherController instances.
+     * @param teacher The Teacher instance.
+     * @param teacherController The TeacherController instance.
+     */
     public TeacherViewKz(Teacher teacher, TeacherController teacherController) {
-        super(teacher,teacherController);
+        super(teacher, teacherController);
     }
 
+    /**
+     * Displays the main menu for the Teacher view in Kazakh and performs actions based on user input.
+     */
     public void getView() {
         System.out.println("Жүйеге қош келдіңіз\n1.Хабарламаны қарау\n2.Барлық хабарламаларды қарау\n3.Хабарлама жіберу\n4.Пәндерді қарау\n5.Студенттерді қарау \n6.Баға қою");
         Scanner sc = new Scanner(System.in);
@@ -36,20 +47,31 @@ public class TeacherViewKz extends TeacherViewEn {
             seeStudents();
         }
     }
-    public void seeMessages(){
-        for(Message message: teacherController.getUnreadMessages(teacher)){
-            System.out.println(message);
-        }
-        getView();
-    }
-    public void seeUnreadMessages(){
-        for(Message message: teacherController.getAllMessages(teacher)){
+
+    /**
+     * Displays unread messages for the Teacher in Kazakh language.
+     */
+    public void seeMessages() {
+        for (Message message : teacherController.getUnreadMessages(teacher)) {
             System.out.println(message);
         }
         getView();
     }
 
-    public void sendMessage(){
+    /**
+     * Displays all messages for the Teacher, including read and unread messages in Kazakh language.
+     */
+    public void seeUnreadMessages() {
+        for (Message message : teacherController.getAllMessages(teacher)) {
+            System.out.println(message);
+        }
+        getView();
+    }
+
+    /**
+     * Composes and sends a message initiated by the Teacher in Kazakh language.
+     */
+    public void sendMessage() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Email жазыңыз: ");
         String email = sc.next();
@@ -65,6 +87,10 @@ public class TeacherViewKz extends TeacherViewEn {
         teacherController.sendMessage(new Message(theme, text, sender, false, LocalDateTime.now()), email);
         getView();
     }
+
+    /**
+     * Displays the courses taught by the Teacher in Kazakh language.
+     */
     public void seeCourses() {
         System.out.println("Мұғалім жүргізетін пәндер:");
         for (String course : teacherController.getAllCourses(teacher)) {
@@ -72,8 +98,9 @@ public class TeacherViewKz extends TeacherViewEn {
         }
     }
 
-
-
+    /**
+     * Displays students enrolled in courses taught by the Teacher in Kazakh language.
+     */
     public void seeStudents() {
         Map<String, Vector<Student>> studentsByCourse = teacherController.getAllStudents(teacher);
         // Вывод студентов по курсам
@@ -84,12 +111,15 @@ public class TeacherViewKz extends TeacherViewEn {
 
             for (Student student : students) {
                 System.out.println("Студент ID-і: " + student.getId() + ", Аты: " + student.getEmail());
-                // Дополнительная информация о студенте, если нужно        }
+                // Дополнительная информация о студенте, если нужно
                 System.out.println();
             }
         }
     }
 
+    /**
+     * Sets marks for students on various assessments in Kazakh language.
+     */
     public void putMarks() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Студент email-ын енгізіңіз:");
@@ -106,5 +136,4 @@ public class TeacherViewKz extends TeacherViewEn {
         Semester semester = Semester.valueOf(scanner.nextLine().toUpperCase());
         teacherController.setMark(teacher, email, new Mark(courseName, attestation1, attestation2, finalExam, semester));
     }
-
 }
