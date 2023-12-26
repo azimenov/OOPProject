fd7f939bf7b85a8f7a84e36c01ae71096a0c245a
Zhanel <122990780+zhanel01@users.noreply.github.com>
2023-12-26 23:19:06 +0600

    Update TeacherRepository.java

--- src/main/java/com/example/oopproject/TeacherService/repository/TeacherRepository.java
+++ src/main/java/com/example/oopproject/TeacherService/repository/TeacherRepository.java
@@ -6,9 +6,33 @@ import com.example.oopproject.TeacherService.Teacher;
 
 import java.util.List;
 
+import java.util.List;
+
 public interface TeacherRepository extends EmployeeRepository {
+
+    /**
+     * Finds a teacher by email.
+     * @param email The email of the teacher.
+     * @return The Teacher object if found, null otherwise.
+     */
     Teacher findTeacherByEmail(String email);
+
+    /**
+     * Finds a teacher by ID.
+     * @param id The ID of the teacher.
+     * @return The Teacher object if found, null otherwise.
+     */
     Teacher findTeacherById(String id);
+
+    /**
+     * Retrieves a list of all teachers.
+     * @return A list containing all the teachers.
+     */
     List<Employee> getAllTeachers();
+
+    /**
+     * Adds a new teacher to the repository.
+     * @param teacher The Teacher object to be added.
+     */
     void addTeacher(Teacher teacher);
 }
