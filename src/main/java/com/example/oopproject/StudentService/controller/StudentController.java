5de436aeda761f7722b6023c3ab66bac47d4ac0d
Baisal <123240140+baimanbk@users.noreply.github.com>
2023-12-26 22:28:51 +0600

    Update StudentController.java

--- src/main/java/com/example/oopproject/StudentService/controller/StudentController.java
+++ src/main/java/com/example/oopproject/StudentService/controller/StudentController.java
@@ -13,15 +13,18 @@ import java.util.Vector;
 
 public class StudentController {
     private final StudentRepositoryImpl studentRepository;
+
+    // Constructor initializing StudentController with StudentRepositoryImpl
     public StudentController(StudentRepositoryImpl studentRepository){
         this.studentRepository = studentRepository;
     }
 
+    // Retrieves groups from a course for a student
     public List<Group> getGroupsFromCourse(Student student, String courseName) {
         return student.getGroups().stream().filter(group -> group.getCourse().getName().equals(courseName)).toList();
     }
 
-
+    // Retrieves all courses for a student
     public HashSet<String> getAllCourses(Student student) {
         HashSet<String> result = new HashSet<String>();
         for (Group group : student.getGroups()) {
@@ -30,6 +33,7 @@ public class StudentController {
         return result;
     }
 
+    // Views information about the teacher for a specific course of a student
     public String viewInfoAboutTeacher(Student student, String courseName){
         for(Group group: student.getGroups()){
             if(group.getCourse().getName().equals(courseName)){
@@ -39,7 +43,23 @@ public class StudentController {
         return null;
     }
 
+    // Views marks of a student
     public Vector<Mark> viewMarks(Student student) {
         return student.getMarks();
+    }
+
+    // Setter for Title of Book
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    // Getter for Availability of Book
+    public boolean getIsAvailable() {
+        return this.isAvailable;
+    }
+    
+    // Setter for Availability of Book
+    public void setIsAvailable(Boolean isAvailable) {
+        this.isAvailable = isAvailable;
     }
 }
