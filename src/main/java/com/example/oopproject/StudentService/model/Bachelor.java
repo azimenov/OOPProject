74e056b66fbc97110ddd8d36fecb9790d19e83de
Baisal <123240140+baimanbk@users.noreply.github.com>
2023-12-26 22:30:59 +0600

    Update Bachelor.java

--- src/main/java/com/example/oopproject/StudentService/model/Bachelor.java
+++ src/main/java/com/example/oopproject/StudentService/model/Bachelor.java
@@ -6,13 +6,15 @@ import com.example.oopproject.UserPackage.enums.Role;
 import com.example.oopproject.StudentService.enums.Faculty;
 import com.example.oopproject.StudentService.enums.Organization;
 
+// Bachelor class extending Student class
 public class Bachelor extends Student {
 
-
+    // Constructor with additional parameters for organization
     public Bachelor(String id, String password, String firstName, String lastName, String phoneNumber, Gender gender, FamilyStatus familyStatus, Role role, Faculty faculty, int yearOfStudy, Organization organization) {
         super(id, password, firstName, lastName, phoneNumber, gender, familyStatus, role, faculty, yearOfStudy, organization);
     }
 
+    // Constructor without organization parameter
     public Bachelor(String id, String password, String firstName, String lastName, String phoneNumber, Gender gender, FamilyStatus familyStatus, Role role, Faculty faculty, int yearOfStudy) {
         super(id, password, firstName, lastName, phoneNumber, gender, familyStatus, role, faculty, yearOfStudy);
     }
