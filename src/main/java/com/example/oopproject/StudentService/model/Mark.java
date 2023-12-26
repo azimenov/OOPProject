5211e01abcccc35132ec7e0d2d567c435d1f0b0f
Baisal <123240140+baimanbk@users.noreply.github.com>
2023-12-26 22:33:24 +0600

    Update Mark.java

--- src/main/java/com/example/oopproject/StudentService/model/Mark.java
+++ src/main/java/com/example/oopproject/StudentService/model/Mark.java
@@ -1,4 +1,5 @@
 package com.example.oopproject.StudentService.model;
+
 import com.example.oopproject.StudentService.enums.GradeLetter;
 import com.example.oopproject.StudentService.enums.Semester;
 
@@ -10,6 +11,7 @@ public class Mark {
     private double finalExam;
     private Semester semester; // Enum representing semester
 
+    // Constructor to initialize a Mark object
     public Mark(String courseName, double attestation1, double attestation2, double finalExam, Semester semester) {
         this.courseName = courseName;
         this.attestation1 = attestation1;
@@ -18,51 +20,62 @@ public class Mark {
         this.semester = semester;
     }
 
-    // Getters and setters for grade, attestation1, attestation2, finalExam, and semester
+    // Getter for grade
     public GradeLetter getGrade() {
         return grade;
     }
 
+    // Setter for grade
     public void setGrade(GradeLetter grade) {
         this.grade = grade;
     }
 
+    // Getter for attestation1
     public double getAttestation1() {
         return attestation1;
     }
 
+    // Setter for attestation1
     public void setAttestation1(double attestation1) {
         this.attestation1 = attestation1;
     }
 
+    // Getter for attestation2
     public double getAttestation2() {
         return attestation2;
     }
 
+    // Setter for attestation2
     public void setAttestation2(double attestation2) {
         this.attestation2 = attestation2;
     }
 
+    // Getter for finalExam
     public double getFinalExam() {
         return finalExam;
     }
 
+    // Setter for finalExam
     public void setFinalExam(double finalExam) {
         this.finalExam = finalExam;
     }
 
+    // Getter for semester
     public Semester getSemester() {
         return semester;
     }
 
+    // Setter for semester
     public void setSemester(Semester semester) {
         this.semester = semester;
     }
 
+    // Getter for courseName
     public String getCourseName() {
         return courseName;
     }
 
+    // Setter for courseName
     public void setCourseName(String courseName) {
         this.courseName = courseName;
     }
