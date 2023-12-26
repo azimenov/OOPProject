cfcf2d46f82f1f3f0d01d947e5a8a720a807b730
Baisal <123240140+baimanbk@users.noreply.github.com>
2023-12-26 22:40:52 +0600

    Update StudentViewRu.java

--- src/main/java/com/example/oopproject/StudentService/view/StudentViewRu.java
+++ src/main/java/com/example/oopproject/StudentService/view/StudentViewRu.java
@@ -5,22 +5,26 @@ import com.example.oopproject.StudentService.model.Mark;
 import com.example.oopproject.StudentService.model.Student;
 
 import java.util.HashSet;
-import java.util.Scanner;
 import java.util.Vector;
 
+// Russian language view class extending the English view
 public class StudentViewRu extends StudentViewEn {
+
+    // Constructor initializing with a StudentController and a Student
     public StudentViewRu(StudentController studentController, Student student) {
-        super(studentController, student );
+        super(studentController, student);
     }
 
-     private void displayRussianView() {
+    // Method to display the Russian view options
+    private void displayRussianView() {
         System.out.println("Добро пожаловать в систему");
         System.out.println("1. Посмотреть курсы");
         System.out.println("2. Посмотреть учителей");
         System.out.println("3. Посмотреть оценки");
-
     }
 
+    // Overridden method to display courses in Russian language
+    @Override
     public void seeCourses() {
         System.out.println("Ваши курсы:");
         HashSet<String> courses = studentController.getAllCourses(student);
@@ -29,6 +33,8 @@ public class StudentViewRu extends StudentViewEn {
         }
     }
 
+    // Overridden method to view teacher information in Russian language
+    @Override
     public void viewInfoAboutTeacher() {
         System.out.print("Введите название курса: ");
         String courseName = scanner.nextLine();
@@ -41,6 +47,8 @@ public class StudentViewRu extends StudentViewEn {
         }
     }
 
+    // Overridden method to view marks in Russian language
+    @Override
     public void viewMarks() {
         System.out.println("Ваши оценки:");
         Vector<Mark> marks = studentController.viewMarks(student);
@@ -49,3 +57,4 @@ public class StudentViewRu extends StudentViewEn {
         }
     }
 }
+
