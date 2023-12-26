09e1eb1d011b31319c06a4ec83a4722b593baa82
Baisal <123240140+baimanbk@users.noreply.github.com>
2023-12-26 22:36:39 +0600

    Update StudentRepository.java

--- src/main/java/com/example/oopproject/StudentService/repository/StudentRepository.java
+++ src/main/java/com/example/oopproject/StudentService/repository/StudentRepository.java
@@ -4,9 +4,17 @@ import com.example.oopproject.StudentService.model.Student;
 
 import java.util.List;
 
+// Interface defining methods for StudentRepository
 public interface StudentRepository {
+    // Finds a student by email
     public Student findStudentByEmail(String email);
+
+    // Finds a student by ID
     public Student findStudentById(String Id);
+
+    // Adds a student to the repository
     public void addStudent(Student student);
+
+    // Retrieves all students from the repository
     public List<Student> getAllStudent();
 }
