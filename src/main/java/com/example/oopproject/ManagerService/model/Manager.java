df8a8b3dec9a623cdd49ad9aeac61c2cd58ed8a3
Zhanel <122990780+zhanel01@users.noreply.github.com>
2023-12-26 22:52:11 +0600

    Update Manager.java

--- src/main/java/com/example/oopproject/ManagerService/model/Manager.java
+++ src/main/java/com/example/oopproject/ManagerService/model/Manager.java
@@ -11,21 +11,50 @@ import com.example.oopproject.db.DataBase;
 import java.io.Serializable;
 import java.util.List;
 
+/**
+ * Manager class represents a Manager, inheriting from the Employee class and implementing Serializable.
+ */
 public class Manager extends Employee implements Serializable {
-	protected List<Message> requests;
-	protected News newsManager;
+    // List of messages associated with requests handled by the manager
+    protected List<Message> requests;
 
-    public Manager(String id, String password, String firstName, String lastName, String phoneNumber, Gender gender, FamilyStatus familyStatus, Role role, int salary) {
+    // News instance managed by the manager
+    protected News newsManager;
+
+    /**
+     * Constructor to initialize a Manager object.
+     * @param id The ID of the manager.
+     * @param password The password of the manager.
+     * @param firstName The first name of the manager.
+     * @param lastName The last name of the manager.
+     * @param phoneNumber The phone number of the manager.
+     * @param gender The gender of the manager.
+     * @param familyStatus The family status of the manager.
+     * @param role The role of the manager.
+     * @param salary The salary of the manager.
+     */
+    public Manager(String id, String password, String firstName, String lastName, String phoneNumber,
+                   Gender gender, FamilyStatus familyStatus, Role role, int salary) {
         super(id, password, firstName, lastName, phoneNumber, gender, familyStatus, role, salary);
     }
 
+    /**
+     * Overrides the toString method to provide a string representation of the Manager object.
+     * @return A string representation of the Manager object.
+     */
     @Override
     public String toString() {
         return "Manager{" +
+                "id='" + getId() + '\'' +
+                ", firstName='" + getFirstName() + '\'' +
+                ", lastName='" + getLastName() + '\'' +
+                ", phoneNumber='" + getPhoneNumber() + '\'' +
+                ", gender=" + getGender() +
+                ", familyStatus=" + getFamilyStatus() +
+                ", role=" + getRole() +
+                ", salary=" + getSalary() +
                 ", requests=" + requests +
                 ", newsManager=" + newsManager +
                 '}';
     }
-   
-
 }
