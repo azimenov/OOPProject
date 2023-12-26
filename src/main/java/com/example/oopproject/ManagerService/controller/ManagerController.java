e553fe74e7bd94656a035c7beefdc4d61254c2b1
Zhanel <122990780+zhanel01@users.noreply.github.com>
2023-12-26 22:52:44 +0600

    Update ManagerController.java

--- src/main/java/com/example/oopproject/ManagerService/controller/ManagerController.java
+++ src/main/java/com/example/oopproject/ManagerService/controller/ManagerController.java
@@ -12,28 +12,66 @@ import com.example.oopproject.StudentService.repository.StudentRepositoryImpl;
 import com.example.oopproject.TeacherService.Teacher;
 import com.example.oopproject.TeacherService.repository.TeacherRepositoryImpl;
 
+/**
+ * ManagerController class, extending from EmployeeController.
+ * Manages operations related to student registration, course assignment, and course activation.
+ */
 public class ManagerController extends EmployeeController {
+    // Repository for managing student-related data
     protected StudentRepositoryImpl studentRepository;
+
+    // Repository for managing course-related data
     protected CourseRepositoryImpl courseRepository;
+
+    // Repository for managing teacher-related data
     protected TeacherRepositoryImpl teacherRepository;
 
-
-    public ManagerController(EmployeeRepositoryImpl repository, StudentRepositoryImpl studentRepository, CourseRepositoryImpl courseRepository, TeacherRepositoryImpl teacherRepository) {
+    /**
+     * Constructor for ManagerController class.
+     * Initializes repositories for employees, students, courses, and teachers.
+     * @param repository EmployeeRepositoryImpl instance to manage employee data.
+     * @param studentRepository StudentRepositoryImpl instance to manage student data.
+     * @param courseRepository CourseRepositoryImpl instance to manage course data.
+     * @param teacherRepository TeacherRepositoryImpl instance to manage teacher data.
+     */
+    public ManagerController(EmployeeRepositoryImpl repository, StudentRepositoryImpl studentRepository,
+                             CourseRepositoryImpl courseRepository, TeacherRepositoryImpl teacherRepository) {
         super(repository);
         this.studentRepository = studentRepository;
         this.courseRepository = courseRepository;
         this.teacherRepository = teacherRepository;
     }
 
+    /**
+     * Registers a student for a course based on email, course name, and group number.
+     * Retrieves the necessary information from repositories to add the student to the course.
+     * @param email Email of the student to be registered.
+     * @param courseName Name of the course for registration.
+     * @param groupNumber Group number for the course registration.
+     */
     public void registerStudentForCourse(String email, String courseName, int groupNumber) {
-        courseRepository.findActivatedCourseByCourseName(courseName).getGroups().elementAt(groupNumber).addStudent(studentRepository.findStudentByEmail(email));
+        courseRepository.findActivatedCourseByCourseName(courseName)
+                .getGroups().elementAt(groupNumber).addStudent(studentRepository.findStudentByEmail(email));
     }
+
+    /**
+     * Assigns a course to a teacher based on email, course name, and group number.
+     * Utilizes repositories to find the necessary information and assigns the course to the teacher.
+     * @param email Email of the teacher to whom the course will be assigned.
+     * @param courseName Name of the course to be assigned.
+     * @param groupNumber Group number for the course assignment.
+     */
     public void assignCourseToTeacher(String email, String courseName, int groupNumber) {
-        courseRepository.findActivatedCourseByCourseName(courseName).assignTeacher(teacherRepository.findTeacherByEmail(email), groupNumber);
-
-
+        courseRepository.findActivatedCourseByCourseName(courseName)
+                .assignTeacher(teacherRepository.findTeacherByEmail(email), groupNumber);
     }
-    public void activateCourse(ActivatedCourses activatedCourses){
+
+    /**
+     * Activates a course using the ActivatedCourses instance provided.
+     * Utilizes the course repository to open/activate the course.
+     * @param activatedCourses ActivatedCourses instance representing the course to be activated.
+     */
+    public void activateCourse(ActivatedCourses activatedCourses) {
         courseRepository.openCourse(activatedCourses);
     }
 }
